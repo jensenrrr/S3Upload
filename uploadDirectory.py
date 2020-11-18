302906d96fcf35d9cf038fa2e88c13f7ef584d7b
jensenrrr <jensen.rr@me.com>
2020-11-18 13:06:55 -0500

    Basic Upload Directory
    * Gets a list of files using python's os module.
    * Upload each file with a s3 compatible object key.
    * This is an unoptimized version.

---
+++ uploadDirectory.py
@@ -0,0 +1,57 @@
+import os
+import boto3
+from boto3.s3.transfer import TransferConfig
+
+S3_BUCKET = ""
+DIR_PATH = r""
+ACCESS_ID = "" 
+ACCESS_KEY=""
+s3_client = boto3.client('s3',
+   aws_access_key_id=ACCESS_ID,
+   aws_secret_access_key= ACCESS_KEY)
+
+rootFolder = os.path.basename(os.path.normpath(DIR_PATH))
+
+'''
+    https://thispointer.com/python-how-to-get-list-of-files-in-directory-and-sub-directories/
+    
+    This will give the list of all the files I need to upload. S3 effectively
+    has a flat file structure (folders are just files with 'folder/' prepended to them) and I can simply
+    upload them using the file path to access the file and a modified version of the filepath to create the
+    object key.
+'''
+
+def getListOfFiles(dirName):
+    # create a list of file and sub directories 
+    # names in the given directory 
+    listOfFile = os.listdir(dirName)
+    allFiles = list()
+    # Iterate over all the entries
+    for entry in listOfFile:
+        # Create full path
+        fullPath = os.path.join(dirName, entry)
+        # If entry is a directory then get the list of files in this directory 
+        if os.path.isdir(fullPath):
+            allFiles = allFiles + getListOfFiles(fullPath)
+        else:
+            allFiles.append(fullPath)
+                
+    return allFiles
+
+def uploadFiles(files, rootFolder, DIR_PATH):
+    for file in files:
+        uploadFileS3(file, rootFolder, DIR_PATH)
+
+'''
+    Documentation:
+        https://boto3.amazonaws.com/v1/documentation/api/latest/guide/s3-uploading-files.html
+'''
+
+def uploadFileS3(filePath, rootFolder, DIR_PATH):
+    objectKey = rootFolder + filePath[len(filePath) - (len(filePath)-len(DIR_PATH)):].replace('\\', '/')
+
+    s3_client.upload_file(filePath, S3_BUCKET, objectKey)
+
+    
+files = getListOfFiles(DIR_PATH)
+uploadFiles(files, rootFolder, DIR_PATH)
